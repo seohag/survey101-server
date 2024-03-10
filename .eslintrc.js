6c79ab83e92c895801baa2b5ef2193806727411f
seohag <sscgeum@gmail.com>
2024-03-10 22:30:48 +0900

    초기 커밋

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}", "./bin/www"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["prettier"],
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+    "no-param-reassign": 0,
+    "no-underscore-dangle": 0,
+    "func-names": "off",
+  },
+};
---
+++ app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const appLoader = require("./src/loaders/index");
+
+const app = express();
+
+(async () => {
+  await appLoader(app);
+})();
+
+module.exports = app;
